3b1c1133f98f2e808aab855f8a2251ed6c6614c5
cockroachj <cackroachj@gmail.com>
2021-03-19 20:46:27 +0800

    1.1.1 Dual Core Line remind LedBlink

--- Socket.ino
+++ Socket.ino
@@ -1,3 +1,5 @@
+#include "soc/soc.h"
+#include "soc/rtc_cntl_reg.h"
 #include <LiquidCrystal_I2C.h>
 #include <ESP32Servo.h>
 #include <WiFi.h>
@@ -11,6 +13,10 @@ int lcdColumns = 16; //LCD行數
 int lcdRows = 2;     //LCD列數
 int count = 0;       //動作次數
 int socketCount = 0; //SOCKET連線次數
+WiFiClientSecure client;
+char host[] = "notify-api.line.me"; //LINE Notify API網址
+String Line_Notify = "QeAvmstuD0REF9qOBY6IAEV4sriSe9VbKeiQTJLqXle";
+
 LiquidCrystal_I2C lcd(0X3F, lcdColumns, lcdRows);
 void getMac()
 { //顯示ESP32 MAC
@@ -107,9 +113,94 @@ void LcdTime()
 unsigned long currentTime = millis();
 unsigned long previousTime = 0;
 const long timeoutTime = 2000;
+bool openFlag = false;
+TaskHandle_t Task1;
+void LedBlink(void *pvParameters)
+{
+    for (;;)
+    {
+        if (WiFi.status() == WL_IDLE_STATUS)
+        {
+            digitalWrite(2, LOW);
+            myServo.write(0);
+            WiFi.begin(ssid, password);
+            while (WiFi.status() != WL_CONNECTED)
+            {
+                Serial.print(".");
+            }
+            LocalIP();
+            wifiServer.begin();
+            EasyDDNS.service("noip");
+            EasyDDNS.client("cackroachj.ddns.net", "cackroachj", "19871019");
+            EasyDDNS.onUpdate([&](const char *oldIP, const char *newIP) {
+                Serial.print("EasyDDNS - IP Change Detected : ");
+                Serial.println(newIP);
+            });
+            delay(1);
+        }else{
+        digitalWrite(2, HIGH);
+        delay(500);
+        digitalWrite(2, LOW);
+        delay(500);
+        }
+    }
+}
+TaskHandle_t Task2;
+void OpenSend(void *pvParameters)
+{
+    for (;;)
+    {
+        if (openFlag == true)
+        {
+            String message = "門被開啟" + dayStamp + "," + timeStamp;
+            if (client.connect(host, 443))
+            {
+                int LEN = message.length();
+                //傳遞POST表頭
+                String url = "/api/notify";
+                client.println("POST " + url + " HTTP/1.1");
+                client.print("Host: ");
+                client.println(host);
+                //權杖
+                client.print("Authorization: Bearer ");
+                client.println(Line_Notify);
+                client.println("Content-Type: application/x-www-form-urlencoded");
+                client.print("Content-Length: ");
+                client.println(String((LEN + 8)));
+                client.println();
+                client.print("message=");
+                client.println(message);
+                client.println();
+                //等候回應
+                delay(1500);
+                String response = client.readString();
+                //顯示傳遞結果
+                Serial.println(response);
+                client.stop(); //斷線，否則只能傳5次
+                openFlag = false;
+            }
+            else
+            {
+                //傳送失敗
+                String response = client.readString();
+                Serial.println(response);
+                Serial.println("connected fail");
+            }
+
+            delay(1000);
+            Serial.println(count);
+        }
+        else
+        {
+            delay(1);
+        }
+    }
+}
 
 void setup()
 {
+    WRITE_PERI_REG(RTC_CNTL_BROWN_OUT_REG, 0);
+    pinMode(2, OUTPUT);
     Serial.begin(115200);
     lcd.init();
     lcd.backlight();
@@ -125,9 +216,7 @@ void setup()
     WiFi.begin(ssid, password);
     while (WiFi.status() != WL_CONNECTED)
     {
-        delay(500);
         Serial.print(".");
-        Text(".", 1);
     }
     Serial.println("");
     Serial.println("WiFi connected.");
@@ -148,9 +237,27 @@ void setup()
         Serial.println(newIP);
     });
     LcdTime();
+    xTaskCreatePinnedToCore(
+      OpenSend, /* Function to implement the task */
+      "Task2", /* Name of the task */
+      10000,  /* Stack size in words */
+      NULL,  /* Task input parameter */
+      0,  /* Priority of the task */
+      &Task2,  /* Task handle. */
+      0); /* Core where the task should run */
+
+      xTaskCreatePinnedToCore(
+      LedBlink, /* Function to implement the task */
+      "Task1", /* Name of the task */
+      10000,  /* Stack size in words */
+      NULL,  /* Task input parameter */
+      0,  /* Priority of the task */
+      &Task1,  /* Task handle. */
+      tskNO_AFFINITY); /* Core where the task should run */
 }
 void loop()
 {
+
     WiFiClient client = wifiServer.available();
     client.setTimeout(3);
     String bufString;
@@ -158,7 +265,7 @@ void loop()
     if (client)
     {
         socketCount++;
-        while (client.connected())
+        if (client.connected())
         {
             Serial.println("client.connected()");
             while (client.available() > 0)
@@ -166,59 +273,43 @@ void loop()
                 char input = client.read();
                 bufString += input;
             }
-
             Serial.println(bufString);
-            // if (bufString.equals("Open"))
-            // {
-            //     myServo.write(90);
-            //     Serial.println("in O");
-            //     Serial.println(bufString);
-            //     LcdTime();
-            //     Text(bufString, 0);
-            // }
-            // else if (bufString.equals("Close"))
-            // {
-            //     myServo.write(0);
-            //     Serial.println("in C");
-            //     Serial.println(bufString);
-            //     LcdTime();
-            //     Text(bufString, 0);
-            //     // delay(3000);
-            // }
-            // else
-            // {
-            //     Serial.println("in default");
-            //     Serial.println(bufString);
-            //     LcdTime();
-            // }
             switch (bufString.charAt(0))
             {
             case 'O':
                 Serial.println("in O");
-                myServo.write(90);
-                LcdTime();
+                openFlag = true;
+                myServo.write(112);
                 count++;
                 client.write("Open");
                 client.flush();
-                // delay(3000);
                 break;
             case 'C':
                 Serial.println("in C");
                 myServo.write(0);
-                LcdTime();
                 count++;
                 client.write("Close");
                 client.flush();
-                // delay(3000);
+                break;
+            case 't':
+                if (bufString.equals("testAdmin"))
+                {
+                    Serial.println("in test");
+                    client.write(count);
+                    client.flush();
+                }
+                else
+                {
+                    client.write("Socket Error...");
+                    client.flush();
+                }
                 break;
             default:
                 Serial.println("in default");
                 client.write("Socket Error...");
                 client.flush();
-                LcdTime();
                 break;
             }
-            break;
         }
         client.stop();
         Serial.println("Client disconnected ! ");
